f8f8b0ab6d608e4c6849692b453d31ab760f107c
baixiangzhu <bxzlovey@163.com>
2016-07-24 11:36:43 +0800

    用webpack构建vue项目完成

---
+++ src/main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by bxz on 2028/7/24.
+ */
+
+import Vue from 'vue'
+
+new Vue({
+    el:'body',
+    data:{
+        message:'Hello Vue !'
+    }
+})
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+/**
+ * Created by bxz on 2028/7/24.
+ */
+
+module.exports = {
+
+    // 这是一个主文件包括其他模块
+    entry:'./src/main.js',
+
+    //编译的文件目录
+    output:{
+        // 'dist'文件夹
+        path:'./dist',
+        //文件 dist/build.js
+        filename:'build.js'
+    },
+    module:{
+        //一些特定的编译规则
+        loaders:[
+            {
+                //让webpack去检验文件是否是.js结尾将其转换
+                test:/\.js$/,
+                //通过babel转换
+                loader:'babel',
+                //不用转换的node_modules文件夹
+                exclude:/node_modules/
+            }
+        ]
+    }
+
+}
